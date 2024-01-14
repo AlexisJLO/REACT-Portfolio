71face3f14ba368b6ebef6c600db9c566d466af0
Alexis Lopez <lopez111alex@gmail.com>
2024-01-13 19:48:51 -0800

    Made about me page

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+export default function About() {
+  return (
+    <div id="about-container" className="about-container">
+      <h1 id="about-title" className="about-title">
+        About Me
+      </h1>
+
+    <section className="about-text">
+      <p>
+        Hello, I am <strong>Alexis Lopez</strong> working as a <strong>Web Developer</strong>. I have <strong>Full-Stack Developer</strong> skills that I learned from the <strong>University of Oregon</strong> Coding Bootcamp. Im currently working towards my bachelor's degree in <strong>Software Development</strong> at <strong>Western Governors University</strong>. Aside from developing. I absolutly endjoy building Computers and playing Volleyball on the weekeneds.
+      </p>
+        </section>
+    </div>
+  );
+}
