d6d7b06ae22ff9edc867e917df95d2ce40223933
Alexis Lopez <lopez111alex@gmail.com>
2024-01-15 22:32:21 -0800

    Updated routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,35 @@
 // Importing ReactDOM for rendering React components
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 // Importing createBrowserRouter and RouterProvider from 'react-router-dom' for routing
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-// Importing Bootstrap CSS for styling
-import "bootstrap/dist/css/bootstrap.min.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Importing the main App component and other page components
-import App from './App.jsx';
-import Error from './pages/Error.jsx';
-import Projects from './pages/Project.jsx';
-import Contact from './pages/Contact.jsx';
-import About from './pages/About.jsx';
-import Resume from './pages/Resume.jsx';
+import App from "./App";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Resume from "./pages/Resume";
 
 // Creating a BrowserRouter instance with route configurations
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />, // Root component for the entire application
-    errorElement: <Error />, // Error component to be displayed on route errors
     children: [
       {
         index: true, // Default route for the root path
         element: <About />, // Component for the About page
       },
       {
-        path: '/Projects',
+        path: "/Projects",
         element: <Projects />, // Component for the Projects page
       },
       {
-        path: '/Resume',
+        path: "/Resume",
         element: <Resume />, // Component for the Resume page
       },
       {
-        path: '/Contact',
+        path: "/Contact",
         element: <Contact />, // Component for the Contact page
       },
     ],
@@ -42,7 +37,7 @@ const router = createBrowserRouter([
 ]);
 
 // Rendering the application using ReactDOM.createRoot
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   // Providing the router to the entire application using RouterProvider
   <RouterProvider router={router} />
 );
